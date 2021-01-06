002bafac8626518692a629b5f583daf6413fe41d
Lisandro Martinez <lisandromartinez@gmail.com>
2021-01-06 09:10:44 -0300

    add test normal item change its properties after update

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,33 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void whenCreatedKnowsItsItems() {
+        Item[] items = new Item[]{new Item("foo", 1, 1)};
+        GildedRose app = new GildedRose(items);
+        assertThat(app.items).containsExactly(items);
+    }
+
+    @Test
+    void whenCreatedItemsPropertiesHaveNoChanged() {
+        Item[] items = new Item[]{new Item("foo", 1, 1)};
+        GildedRose app = new GildedRose(items);
+        assertThat(app.items[0].quality).isEqualTo(1);
+        assertThat(app.items[0].sellIn).isEqualTo(1);
+    }
+
+    @Test
+    void changeNormalItemPropertiesAfterUpdate() {
+        Item[] items = new Item[]{new Item("foo", 1, 1)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertThat(app.items[0].quality).isZero();
+        assertThat(app.items[0].sellIn).isZero();
     }
+
 
 }
