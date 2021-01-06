d5aa31a3cb95b41da4b41a3cd1c8100e314d5822
Lisandro Martinez <lisandromartinez@gmail.com>
2021-01-06 11:19:05 -0300

    delete non-java projects

---
+++ Java/src/test/java/com/gildedrose/AgedBrieItemGildedRoseTest.java
@@ -0,0 +1,14 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AgedBrieItemGildedRoseTest {
+    private ObjectCreator creator = new ObjectCreator();
+
+    @Test
+    void XX() {
+        fail();
+    }
+}
